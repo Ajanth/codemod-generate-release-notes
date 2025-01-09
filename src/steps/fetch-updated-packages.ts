92dbb37407c21a183b3b511495077e2e2e98abf2
Ajanth <ajanth2newearth@gmail.com>
2025-01-09 11:40:38 +0100

    refactor: get changes since last tag

--- src/steps/fetch-updated-packages.ts
+++ src/steps/fetch-updated-packages.ts
@@ -5,26 +5,15 @@ import { readPackageJson } from '@codemod-utils/json';
 
 import { Options, PackageNameVersionEntry } from '../types/index.js';
 
-// Step: Fetch Updated Packages from the Latest Commit's CHANGELOG.md Files
+// Step: Fetch Updated Packages from the Latest Commit's package.json Files
 export function fetchUpdatedPackages(
   options: Options,
 ): PackageNameVersionEntry[] {
-  const { packagesPath, projectRoot } = options;
+  const { projectRoot } = options;
 
-  let changedPackageJsonFiles = packageJsonFilesFromLastCommit(
-    packagesPath,
-    projectRoot,
-  );
+  const changedPackageJsonFiles = packageJsonFilesSinceLastTag(projectRoot);
+
   const updatedPackages: PackageNameVersionEntry[] = [];
-
-  changedPackageJsonFiles = changedPackageJsonFiles.filter((filePath) => {
-    // we skip package.json files in the root since we don't include them in the release notes
-    // to review later if this can be an option
-    if (filePath === '/package.json') {
-      return false;
-    }
-    return true;
-  });
 
   for (const filePath of changedPackageJsonFiles) {
     const fullPath = join(projectRoot, filePath.replace('package.json', ''));
@@ -41,25 +30,20 @@ export function fetchUpdatedPackages(
   return updatedPackages;
 }
 
-function packageJsonFilesFromLastCommit(
-  packagesPath: string,
-  projectRoot: string,
-): string[] {
-  const lastCommitHash = execSync('git rev-parse HEAD', { cwd: projectRoot })
+function packageJsonFilesSinceLastTag(projectRoot: string): string[] {
+  const lastTag = execSync('git describe --tags --abbrev=0', {
+    cwd: projectRoot,
+  })
     .toString()
     .trim();
 
-  const changedFilesOutput = execSync(
-    `git diff-tree --no-commit-id --name-only -r ${lastCommitHash}`,
-    { cwd: projectRoot },
-  )
+  const changedFilesOutput = execSync(`git diff --name-only ${lastTag} HEAD`, {
+    cwd: projectRoot,
+  })
     .toString()
     .trim();
 
   const changedFiles = changedFilesOutput ? changedFilesOutput.split('\n') : [];
 
-  return changedFiles.filter(
-    (filePath) =>
-      filePath.startsWith(packagesPath) && filePath.endsWith('package.json'),
-  );
+  return changedFiles.filter((filePath) => filePath.endsWith('package.json'));
 }
