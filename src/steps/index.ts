91c6f6b52a48bf0a0d2b2e50aea7a463ce086bbb
Ajanth Kathirkamu <ajanth2newearth@gmail.com>
2025-01-18 20:36:47 +0100

    Feat: Add pnpm update script in release notes (#7)
    * feat: add step to generate update command for packages
    
    * feat: include update script in release notes generation
    
    * test: update test to verify new changes
    
    * Revert "Feat: provide pnpm update command in release notes" (#6)
    
    * feat: add step to generate update command for packages
    
    * feat: include update script in release notes generation
    
    * test: update test to verify new changes
    
    * chore: added changeset

--- src/steps/index.ts
+++ src/steps/index.ts
@@ -1,4 +1,5 @@
 export * from './create-options.js';
 export * from './create-release-notes.js';
 export * from './get-latest-versions.js';
+export * from './get-update-script.js';
 export * from './get-updated-packages.js';
